f1e97c3cc8846d500e48b0c0863438a3cda25c56
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-01-29 02:23:05 +0330

    headerbackground

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -2,26 +2,31 @@ import Link from "next/link"
 import logo from '../assets/logo.png'
 import classes from './mainHeader.module.css'
 import Image from "next/image"
+import HeaderBackground from "./HeaderBackground"
 
 const MainHeader = () => {
     return (
-        <header className={classes.header}>
-            <Link href='/' className={classes.logo} >
-                <Image src={logo} alt="logo" />
-                Next Food App
-            </Link>
+        <>
+            <HeaderBackground />
+            <header className={classes.header}>
+                <Link href='/' className={classes.logo} >
+                    <Image src={logo} alt="logo" priority='true'
+                    />
+                    Next Food App
+                </Link>
 
-            <nav className={classes.nav}>
-                <ul>
-                    <li>
-                        <Link href='/meals'>Browse Meals</Link>
-                    </li>
-                    <li>
-                        <Link href='/community'>Food Community</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href='/meals'>Browse Meals</Link>
+                        </li>
+                        <li>
+                            <Link href='/community'>Food Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
