269a0d9d084f73b045982c96b94bddada430c9d1
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-01 03:46:32 +0330

    slide show and main header

--- app/page.js
+++ app/page.js
@@ -1,20 +1,52 @@
 import Link from 'next/link';
-
+import classes from './page.module.css';
+import ImageSlideshow from '@/components/images/image-slideshow';
 export default function Home() {
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-      <p>
-        <Link href='/meals'>meals page</Link>
-      </p>
-      <p>
-        <Link href='/community'>community page</Link>
-      </p>
-      <p>
-        <Link href='/meals/share'>share page</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
+
+        <div>
+          <div className={classes.hero}>
+            <h1>Next Food App</h1>
+            <p>Taste and Share Food From All Over The World</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href='/community'>join the community</Link>
+            <Link href='/meals'>Explore Melas</Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            Food App is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            Food App is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+
+        <section className={classes.section}>
+          <h2>Why Food App?</h2>
+          <p>
+            Food App is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes, and to
+            connect with other food lovers.
+          </p>
+          <p>
+            Food App is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
+    </>
   );
 }
--- components/main-header/MainHeader.jsx
+++ components/main-header/MainHeader.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import logo from '../assets/logo.png'
+import logo from '../../assets/logo.png'
 import classes from './mainHeader.module.css'
 import Image from "next/image"
 import HeaderBackground from "./HeaderBackground"
