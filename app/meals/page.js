cdadea834773124f8e60f25170e3c04a2c1d2c8e
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-02-09 20:48:31 +0330

    meals section

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,28 @@
+import Link from 'next/link';
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/MealsGrid';
+
 export default function MealsPage() {
   return (
     <>
-      <h1>meals page</h1>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{' '}
+          <spam className={classes.highlight}>By you</spam>
+        </h1>
+
+        <p>
+          Choose your favorite recipe and cook it your self. it is easy and fun
+        </p>
+
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 }
